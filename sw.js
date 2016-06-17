94a28afa48386b80998ea8499e7601bc8b7831d5
Vincent De Oliveira <vincent@iamvdo.me>
2016-06-17 15:50:50 +0200

    Add public folder

---
+++ sw.js
@@ -0,0 +1,30 @@
+// install
+// create cache and store all assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('speedometer')
+      .then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/app.css',
+          '/app.js',
+          '/vendor.js',
+          '/manifest.json'
+       ]);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
+});
+
+// fetch
+// cache first
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
